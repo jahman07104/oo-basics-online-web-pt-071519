784918bcbe115efdb28a8f9b30e871703c2bb7f1
Patrick Harrison <jahman@bellatlantic.net>
2019-08-15 18:54:11 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -16,17 +16,4 @@ class Book
     @page_count = num
   end
  
-  def page_count
-    @page_count
-  end
-    def genre=(genre)
-    @genre = genre
-  end
- 
-  def genre
-    @genre
-  end
-  def turn_page
-        puts "Flipping the page...wow, you read fast!"
-  end
-end
+
